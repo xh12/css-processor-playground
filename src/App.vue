a476f5289c3d9fa78164a4404d146b73635b9f71
xh12 <1960939421@qq.com>
2022-09-21 11:52:52 +0000

    修复输入问题

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,62 @@
 <script setup>
-  import Code from './components/code.vue'
-  import { ref, watch } from 'vue'
-  import snippet from './snippet.mjs'
-  let type = 'variable'
-  const language = ref('scss')
-  const from = ref(snippet[type][language.value])
-  const to = ref('')
+import Code from './components/code.vue'
+import { ref } from 'vue'
+import snippet from './snippet.mjs'
 
-  watch(from, (value) => {
-    if (language.value === 'scss') {
-      fetch('/api/compile', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify({
-          compiler: "dart-sass/1.32.12",
-          input: value,
-          outputStyle: 'expanded',
-          syntax: 'SCSS'
-        })
-      }).then(async (v) => { 
-        const res = await v.json()
-        to.value = res.css 
+let type = 'variable'
+const language = ref('scss')
+const from = ref(snippet[type][language.value])
+const to = ref('');
+
+
+renderToCss(from.value)
+
+function ontypeselect(e) {
+  type = e.target.value
+  from.value = snippet[type][language.value]
+}
+
+function onlangselect(e) {
+  language.value = e.target.value
+  from.value = snippet[type][language.value]
+}
+
+function onchange(value) {
+  renderToCss(value)
+}
+function renderToCss(value) {
+  const lang = language.value;
+  if (lang === 'scss') {
+    // console.log(sass);
+    // const res = sass.compileString(value)
+    // console.log(res);
+    fetch('/api/compile', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({
+        compiler: "dart-sass/1.32.12",
+        input: value,
+        outputStyle: 'expanded',
+        syntax: 'SCSS'
       })
-    } else {
-      console.log('less change')
-      window.less.render(value)
-        .then(v => {
-          to.value = v.css
-        })
-    }
-  }, { immediate: true })
-
-  function ontypeselect(e) {
-    type = e.target.value
-    from.value = snippet[type][language.value]
+    }).then(async (v) => {
+      const res = await v.json()
+      to.value = res.css
+      // emit('change', res.css)
+    })
+  } else {
+    console.log('less change')
+    window.less.render(value)
+      .then(v => {
+        to.value = v.css
+        // emit('change', v.css)
+      })
   }
-
-  function onlangselect(e) {
-    language.value = e.target.value
-    from.value = snippet[type][language.value]
-  }
-
-  function onchange(value) {
-    console.log(value)
-    from.value = value
-  }
+}
 </script>
 
 <template>
@@ -65,21 +73,23 @@
       </select>
     </header>
     <div class="code-container"><Code :value="from" :lang="language" @change="onchange" /></div>
-    <div class="code-container"><Code :value="to"/></div>
+    <div class="code-container"><Code :value="to" /></div>
   </div>
 </template>
 
 <style scoped>
-  .container:after {
-    content: "";
-    display: block;
-    clear: both;
-  }
-  .code-container {
-    width: 50%;
-    float: left;
-  }
-  header {
-    height: 5vh;
-  }
+.container:after {
+  content: "";
+  display: block;
+  clear: both;
+}
+
+.code-container {
+  width: 50%;
+  float: left;
+}
+
+header {
+  height: 5vh;
+}
 </style>
--- src/components/code.vue
+++ src/components/code.vue
@@ -1,59 +1,62 @@
 <script setup>
-    import * as monaco from 'monaco-editor';
-    import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-    import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-    import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-    import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-    import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-    import { getCurrentInstance, onMounted, watch } from 'vue'
-    self.MonacoEnvironment = {
-        getWorker(_, label) {
-            if (label === 'json') {
-                return new jsonWorker()
-            }
-            if (label === 'css' || label === 'scss' || label === 'less') {
-                return new cssWorker()
-            }
-            if (label === 'html' || label === 'handlebars' || label === 'razor') {
-                return new htmlWorker()
-            }
-            if (label === 'typescript' || label === 'javascript') {
-                return new tsWorker()
-            }
-            return new editorWorker()
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import { getCurrentInstance, onMounted, watch, watchEffect } from 'vue'
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === 'json') {
+            return new jsonWorker()
         }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+            return new cssWorker()
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return new htmlWorker()
+        }
+        if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker()
+        }
+        return new editorWorker()
     }
-    const props = defineProps({
-        value: String,
-        lang: String,
-    })
-    const emit = defineEmits(['change'])
+}
+const props = defineProps({
+    value: String,
+    lang: String,
+})
+const emit = defineEmits(['change']);
 
-    onMounted(() => {
-        const instance = getCurrentInstance()
-        const { root } = instance.refs
-        const editor = monaco.editor.create(root, {
-            value: props.value,
-            language: props.lang || 'css',
-            fontSize: 15
-        })
-        watch(() => props.value || '', (value) => {
-            editor.setValue(value)
-        })
-        watch(() => props.lang || 'css', (value) => {
-            console.log('lang === ', value)
-            const model = monaco.editor.createModel(props.value, value) 
-            editor.setModel(model)
-        })
-        editor.onDidChangeModelContent(() => {
-            emit('change', editor.getValue())
-        })
+
+onMounted(() => {
+    const instance = getCurrentInstance()
+    const { root } = instance.refs
+    const editor = monaco.editor.create(root, {
+        value: props.value,
+        language: props.lang || 'css',
+        fontSize: 15
     })
+    watch(() => props.value || '', (value) => {
+    console.log(value, '');
+        editor.setValue(value)
+    })
+    watch(() => props.lang || 'css', (value) => {
+        const model = monaco.editor.createModel(props.value, value)
+        editor.setModel(model)
+    })
+    editor.onDidChangeModelContent(() => {
+        // renderToCss(editor.getValue())
+        emit('change', editor.getValue())
+    })
+})
+
 </script>
 <style scoped>
-    .code-root {
-        height: 95vh;
-    }
+.code-root {
+    height: 95vh;
+}
 </style>
 <template>
     <div class="code-root" ref="root"></div>
